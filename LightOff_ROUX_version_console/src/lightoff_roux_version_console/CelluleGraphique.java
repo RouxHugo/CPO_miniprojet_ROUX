dbfd318f90c9cb1e5c7a84c11b8e6ec69cd66e8c
hugo roux <hugo.roux1@epfedu.fr>
2024-12-02 15:33:31 +0100

    Ajout de la classe CelluleGraphique avec ses attributs et constructeur - Création de la classe CelluleGraphique héritant de JButton. - Ajout des attributs :   - celluleLumineuseAssociee (CelluleLumineuse associée).   - largeur (largeur en pixels).   - longueur (longueur en pixels). - Implémentation d'un constructeur pour initialiser ces attributs.

---
+++ LightOff_ROUX_version_console/src/lightoff_roux_version_console/CelluleGraphique.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightoff_roux_version_console;
+
+/**
+ *
+ * @author hugor
+ */
+import javax.swing.*;
+import java.awt.*;
+
+public class CelluleGraphique extends JButton {
+
+    // Attributs
+    private CelluleLumineuse celluleLumineuseAssociee;
+    private int largeur;
+    private int longueur;
+
+    // Constructeur
+    public CelluleGraphique(CelluleLumineuse celluleLumineuseAssociee, int largeur, int longueur) {
+        this.celluleLumineuseAssociee = celluleLumineuseAssociee;
+        this.largeur = largeur;
+        this.longueur = longueur;
+        
+    }
+}
